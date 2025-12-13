4fa4a6b07160b6e0dca410aade8446ee79a8580a
Colin Duffy <colin@tomorrowevening.com>
2025-12-13 15:26:27 -0800

    fix(dev): keep true for demo

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
 // @ts-ignore
-export const IS_DEV = import.meta.env.MODE === 'development';
+// export const IS_DEV = import.meta.env.MODE === 'development';
+export const IS_DEV = true;
 export const IS_EDITOR = IS_DEV && document.location.hash.search('editor') > -1;
